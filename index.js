484648fee98dd6645c3c0f52ea84a51197ea2a29
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-01-26 12:17:00 +0200

    get btn element and create getQuote function

---
+++ index.js
@@ -0,0 +1,12 @@
+//DOM Manipulation
+
+const btnEl = document.getElementById("btn");
+console.log(btnEl);
+
+//function
+function getQuote() {
+  console.log("clickeds");
+  console.log("clicked");
+}
+
+btnEl.addEventListener("click", getQuote);
