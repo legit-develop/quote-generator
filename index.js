bfdc12460619a8800d9a7573e121a6c8139ddc49
Sifiso Dhlamini <sb.dhlamini@gmail.com>
2023-01-26 12:33:36 +0200

    get quote and author element and use innerText to insert data from api

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 //DOM Manipulation
 
 const btnEl = document.getElementById("btn");
-console.log(btnEl);
+//console.log(btnEl);
+const apiURL = "https://api.quotable.io/random";
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
 
 //function
-function getQuote() {
-  console.log("clickeds");
-  console.log("clicked");
+async function getQuote() {
+  //fetch method
+  const response = await fetch(apiURL);
+  //convert to json
+  const data = await response.json();
+  // console.log(data);
+  const quoteContent = data.content;
+  const quoteAuthor = data.author;
+  //console.log(quoteContent);
+  //console.log(quoteAuthor);
+  quoteEl.innerText = quoteContent;
+  authorEl.innerText = `~ ${quoteAuthor}`;
 }
 
 btnEl.addEventListener("click", getQuote);
